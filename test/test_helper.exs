2a9fec274f7e97c494d8466afef4ae2a69164a2d
Dmitry Ledentsov <dmlled@yahoo.com>
2025-10-14 08:12:00 +0200

    no slow tests in the main pipeline

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 # Exclude diagram generation tests by default to avoid generating HTML files
 # Run them explicitly with: mix test --include diagram_generation
-ExUnit.start(exclude: [:diagram_generation])
+# Exclude slow tests by default (they can be run with: mix test --include slow)
+ExUnit.start(exclude: [:diagram_generation, :slow])
 
 # Configure JUnit formatter for CI
 if System.get_env("CI") do
