41a3a0f1d31318ebe7a1072353744df793e2c151
Dmitry Ledentsov <dmlled@yahoo.com>
2025-10-25 12:45:02 +0200

    rc1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GenServerVirtualTime.MixProject do
   use Mix.Project
 
-  @version "0.5.0-rc.0"
+  @version "0.5.0-rc.1"
   @source_url "https://github.com/d-led/gen_server_virtual_time"
 
   def project do
